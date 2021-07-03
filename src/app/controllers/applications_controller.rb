adbf90c7b3b0a42b0dd28bb8030b5484174a6e19
masaki2355 <takoyaki2355@icloud.com>
2021-07-03 15:15:55 +0900

    rails

---
+++ src/app/controllers/applications_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationsController < ActionController::Base
+  def hello
+    @hello = "こんにちは"
+    @message = "helloアクションが実行されています"
+  end
+
+  def call
+    @call = "callアクションが実行されています"
+  end
+end
